2a688144820eb644b07aa37908ac17927bbe266d
anavla777 <w.ananenkow@gmail.com>
2024-06-30 14:30:29 +0300

    fixes in test for low resolutions

--- src/test/java/pages/ProfilePage.java
+++ src/test/java/pages/ProfilePage.java
@@ -24,7 +24,7 @@ public class ProfilePage {
 
     @Step("Delete book from the list")
     public ProfilePage deleteBook() {
-        deleteButton.click();
+        deleteButton.scrollIntoView(true).click();
         return this;
     }
 
